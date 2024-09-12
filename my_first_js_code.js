ec7021bbb87b5634cb3da01b00dbaba049edb75f
Katawera Noel Ephraim <noelkatawera@gmail.com>
2024-09-12 17:33:57 +0300

    Add files via upload
    week 2

---
+++ my_first_js_code.js
@@ -0,0 +1,35 @@
+//my first js code
+
+console.log("Hello world")
+//
+
+console.log("Katawera Noel Ephraim");
+console.log("B27030");
+console.log("M24B13/013");
+console.log("BSIT");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ variables.js
@@ -0,0 +1,33 @@
+//variables
+
+
+
+//using var
+
+// inintialization
+var my_name = "Arcane Wildcrd";
+
+//declare a variable
+var height;
+
+
+
+//assign a variable
+height = 30.13; 
+
+console.log("my_name");
+
+console.log("height");
+
+console.log(my_name," - ",height)
+
+//let
+let area = 90.5;
+console.log("Area", area)
+let f_name;
+f_name = "arcane";
+console.log("first Name", f_name)
+
+
+//const
+const pie = 3.14
